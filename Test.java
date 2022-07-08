6952a1f7c8cadd1f6da7858e3363b3691a008bb4
Onder-Onur <108507281+Onder-Onur@users.noreply.github.com>
2022-07-08 00:13:26 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is my firts Repository");
         System.out.println("Fetch Test")
+        System.out.println("Fetch Test1")
+            System.out.println("Fetch Test")
     }
 }
